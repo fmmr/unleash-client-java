59daf7ea60802c5cd092ab172c051849faf6f1e1
frerodla <fmr@finn.no>
2015-08-21 09:46:50 +0200

    issue-102 adedd javadoc & comment on java8-impl

--- src/main/java/no/finn/unleash/strategy/AggregateStrategy.java
+++ src/main/java/no/finn/unleash/strategy/AggregateStrategy.java
@@ -3,6 +3,10 @@ package no.finn.unleash.strategy;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Contains a list of other stratetgy.  <code>isEnabled</code> is <code>true</code> if <strong>any</strong> of the strategies
+ * are <code>true</code>.
+ */
 public abstract class AggregateStrategy implements Strategy {
 
     protected final List<Strategy> strategies;
@@ -12,6 +16,8 @@ public abstract class AggregateStrategy implements Strategy {
     }
 
     public boolean isEnabled(Map<String, String> parameters) {
+        // if this is a java8 projects the following could be used
+        // strategies.stream().filter(strategy -> strategy.isEnabled(parameters)).findAny().isPresent();
         for (Strategy strategy : strategies) {
             if (strategy.isEnabled(parameters)) {
                 return true;
